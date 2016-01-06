74514a353d9a3642a19edc4cf39a772285938e11
Andrew Keller <andrew.keller.09@gmail.com>
2016-01-05 18:13:52 -0800

    Deployed 37bbc10 with MkDocs version: 0.14.0

---
+++ build.jl
@@ -0,0 +1,10 @@
+using Lexicon, PainterQB
+
+cd(dirname(@__FILE__))
+
+docs = [save("api/PainterQB.md",PainterQB;md_subheader=:category)]
+push!(docs,save("api/E5071C.md",PainterQB.E5071CModule;md_subheader=:category))
+push!(docs,save("api/E8257D.md",PainterQB.E8257DModule;md_subheader=:category))
+push!(docs,save("api/AWG5014C.md",PainterQB.AWG5014CModule;md_subheader=:category))
+push!(docs,save("api/AlazarTech.md",PainterQB.AlazarModule;md_subheader=:category))
+save("api/api.md",Index(docs);md_subheader=:category);
