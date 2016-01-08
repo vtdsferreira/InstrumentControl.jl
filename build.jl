fe5ed230e58da90cacfbde9936ed44cd8db7ae08
Andrew Keller <andrew.keller.09@gmail.com>
2016-01-07 18:52:32 -0800

    Deployed ef9c7ad with MkDocs version: 0.14.0

--- build.jl
+++ build.jl
@@ -1,10 +1,12 @@
-using Lexicon, PainterQB
+using PainterQB, Lexicon
 
 cd(dirname(@__FILE__))
 
-docs = [save("api/PainterQB.md",PainterQB;md_subheader=:category)]
-push!(docs,save("api/E5071C.md",PainterQB.E5071CModule;md_subheader=:category))
-push!(docs,save("api/E8257D.md",PainterQB.E8257DModule;md_subheader=:category))
-push!(docs,save("api/AWG5014C.md",PainterQB.AWG5014CModule;md_subheader=:category))
-push!(docs,save("api/AlazarTech.md",PainterQB.AlazarModule;md_subheader=:category))
-save("api/api.md",Index(docs);md_subheader=:category);
+docs = [save("api/PainterQB.md",PainterQB; md_subheader=:category)]
+
+push!(docs, save("api/E5071C.md", PainterQB.E5071CModule; md_subheader=:category))
+push!(docs, save("api/E8257D.md", PainterQB.E8257DModule; md_subheader=:category))
+push!(docs, save("api/AWG5014C.md", PainterQB.AWG5014CModule; md_subheader=:category))
+push!(docs, save("api/AlazarTech.md", PainterQB.AlazarModule; md_subheader=:category))
+
+save("api/api.md", Index(docs); md_subheader=:category);
