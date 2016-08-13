7ec4b5a84485eb7764c93fbd30896861f198a54a
Andrew Keller <andrew.keller.09@gmail.com>
2016-08-13 15:13:31 -0700

    Add basic SMB100A support

---
+++ src/instruments/SMB100A.jl
@@ -0,0 +1,15 @@
+module SMB100A
+import Base: getindex, setindex!
+import VISA
+importall InstrumentControl     # All the stuff in InstrumentDefs, etc.
+
+returntype(::Type{Bool}) = Int
+returntype(::Type{Real}) = Float64
+returntype(::Type{Integer}) = Int
+fmt(v::Bool) = string(Int(v))
+fmt(v) = string(v)
+
+metadata = insjson(joinpath(Pkg.dir("InstrumentControl"),"deps/SMB100A.json"))
+generate_all(metadata)
+
+end
