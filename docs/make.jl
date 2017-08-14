3e75bb51608ac74a11983adff3df37fb3f06fedf
Andrew Keller <andrew.keller.09@gmail.com>
2017-08-14 10:50:57 -0700

    Set up documentation autobuild.

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,10 @@
 using Documenter, InstrumentControl
 
 makedocs()
+
+deploydocs(
+    deps   = Deps.pip("mkdocs", "python-markdown-math"),
+    repo   = "github.com/PainterQubits/InstrumentControl.jl.git",
+    julia  = "0.6",
+    osname = "linux"
+)
